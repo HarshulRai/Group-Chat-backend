bc14aa68aa156537fa69f95a414cb807e43be66c
Harshul Rai <61915312+HarshulRai@users.noreply.github.com>
2022-08-10 23:55:35 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const bodyParser = require('body-parser')
 const sequelize = require('./util/database')
 
 const userRoutes = require('./routes/user')
+const messageRoutes = require('./routes/message')
 
 const User = require('./models/user')
+const Message = require('./routes/message')
 
 const app = express()
 
@@ -15,6 +17,10 @@ app.use(cors())
 app.use(bodyParser.json())
 
 app.use(userRoutes)
+app.use(messageRoutes)
+
+// User.hasMany(Message)
+// Message.belongsTo(User)
 
 sequelize.sync()
 .then((res)=>{
